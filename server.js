c00edebdb6e7f1c9d844658437cc0374816c4f1c
arrow913 <billy.arrasmith@gmail.com>
2020-10-14 20:25:59 -0500

    merging main

--- server.js
+++ server.js
@@ -1,49 +1,61 @@
 var express = require("express");
 
+
 var app = express();
-var PORT = process.env.PORT || 3000;
 var PORT = 3000;
 
-app.use(express.urlencoded({ extended: true 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
-var tables = [{
+var reservation = [{
   routeName: "reservation",
-  name: " ",
-  phoneNumber: " ",
-  Email: " ",
-  forcePoints: 2000
- 
-  //   routeName: "darthmaul",
-  //   name: "Darth Maul",
-  //   role: "Sith Lord",
-  //   age: 200,
-  //   forcePoints: 1200
-  // }, {
-  //   routeName: "obiwankenobi",
-  //   name: "Obi Wan Kenobi",
-  //   role: "Jedi Knight",
-  //   age: 60,
-  //   forcePoints: 1350
-  }];
+  name: "name",
+  phoneNumber: "phone number",
+  Email: "email",
+  uniqueID: "id"
+}];
 
-  // // Routes
-  // // ===========================================================
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-  });
+var waitlist = [{
+  routeName: "waitlist",
+  name: "name",
+  phoneNumber: "phone number",
+  Email: "email",
+  uniqueID: "id"
+}];
 
-  app.get("/:character", function (req, res) {
-    var chosen = req.params.character;
 
-    // What does this log?
-       console.log(chosen);
 
-    //   res.end();
- });
+
+// Routes
+// ===========================================================
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/api/reservation", function(req, res) {
+  return res.json(reservation);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitlist);
+});
+
+app.post("/api/reservation", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newReservation = req.body;
+
+  console.log(newReservation);
+
+  // We then add the json the user sent to the character array
+  reservation.push(newReservation);
+
+  // We then display the JSON to the users
+  res.json(newReservation);
+});
+
+
+  
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
