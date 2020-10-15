ef0d4d168c86caa51154b10abb648c01454f6075
arrow913 <billy.arrasmith@gmail.com>
2020-10-14 20:13:27 -0500

    nothing

--- server.js
+++ server.js
@@ -1,44 +1,49 @@
 var express = require("express");
 
 var app = express();
+var PORT = process.env.PORT || 3000;
 var PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
 
-// var tables = [{
-//   routeName: "reservation",
-//   name: " ",
-//   phoneNumber: " ",
-//   Email: " ",
-//   forcePoints: 2000
-// }, {
-//   routeName: "darthmaul",
-//   name: "Darth Maul",
-//   role: "Sith Lord",
-//   age: 200,
-//   forcePoints: 1200
-// }, {
-//   routeName: "obiwankenobi",
-//   name: "Obi Wan Kenobi",
-//   role: "Jedi Knight",
-//   age: 60,
-//   forcePoints: 1350
-// }];
 
-// // Routes
-// // ===========================================================
-// app.get("/", function(req, res) {
-//   res.send("Welcome to the Star Wars Page!");
-// });
 
-// app.get("/:character", function(req, res) {
-//   var chosen = req.params.character;
+var tables = [{
+  routeName: "reservation",
+  name: " ",
+  phoneNumber: " ",
+  Email: " ",
+  forcePoints: 2000
+ 
+  //   routeName: "darthmaul",
+  //   name: "Darth Maul",
+  //   role: "Sith Lord",
+  //   age: 200,
+  //   forcePoints: 1200
+  // }, {
+  //   routeName: "obiwankenobi",
+  //   name: "Obi Wan Kenobi",
+  //   role: "Jedi Knight",
+  //   age: 60,
+  //   forcePoints: 1350
+  }];
 
-//   // What does this log?
-//   console.log(chosen);
+  // // Routes
+  // // ===========================================================
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 
-//   res.end();
-// });
+  app.get("/:character", function (req, res) {
+    var chosen = req.params.character;
+
+    // What does this log?
+       console.log(chosen);
+
+    //   res.end();
+ });
